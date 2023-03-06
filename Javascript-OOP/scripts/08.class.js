b5f8c428304ba6cac60e298241b7decc965cec09
Stevanus Pangau <stevanuspangau50@gmail.com>
2023-03-06 20:58:25 +0700

    class

---
+++ Javascript-OOP/scripts/08.class.js
@@ -0,0 +1,11 @@
+/**
+Sejak EcmaScript versi 6, diperkenalkan kata kunci baru, yaitu class, ini merupakan kata kunci yang digunakan untuk membuat class di JavaScript
+Dengan kata kunci class, kita tidak perlu lagi menggunakan constructor function untuk membuat class
+ */
+
+class Person {
+  //
+}
+
+const evan = new Person();
+console.info(evan);
