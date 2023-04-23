6a38262310e35c43c56e2bb5a873772d661ae2f4
Stevanus Pangau <stevanuspangau50@gmail.com>
2023-04-23 22:03:48 +0700

    multiple export

---
+++ Javascript-Modules/scripts/5.multiple-export.mjs
@@ -0,0 +1,10 @@
+const company = 'Programmer Mitra';
+
+function sum(first, second) {
+  return first + second;
+}
+
+class Company {}
+
+// contoh langsung export semua tanpa di tulis export satu" di tiap function, variable atau class
+export { company, sum, Company };
