255f7816df816d4dcfb90214c7538fe2cbb05807
Stevanus Pangau <stevanuspangau50@gmail.com>
2023-04-23 22:45:02 +0700

    Module Object

---
+++ Javascript-Modules/scripts/8.module-object.mjs
@@ -0,0 +1,13 @@
+export const firstName = 'Stevanus';
+export const middleName = 'Evan';
+export const lastName = 'Pangau';
+
+export function hello() {
+  console.info(`Hello from Evan`);
+}
+
+export class Person {
+  constructor() {
+    this.name = 'Tivani';
+  }
+}
