ef3710a963f6193bc5ea2baf10dc3c354266739d
Stevanus Pangau <stevanuspangau50@gmail.com>
2023-04-23 22:15:40 +0700

    Alias

---
+++ Javascript-Modules/scripts/6.alias.mjs
@@ -0,0 +1,12 @@
+const company = 'Programmer Mitra';
+
+function sum(first, second) {
+  return first + second;
+}
+
+class Company {}
+
+// bisa di aliaskan saat di export, tapi ini tidak di rekomendasikan
+// export { company as perusahaan, sum as total, Company as Perusahaan };
+
+export { company, sum, Company };
